881f375fac92d0342500997d0b41ea5630033f87
AKSHAY <akshaybhandare08@gmail.com>
2019-03-02 21:44:02 +0530

    Text Recognition Added

--- app/src/main/java/com/example/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/libraryapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.libraryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import io.card.payment.CardIOActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button regbutton1;
+
+    protected static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_SCAN = 100;
+    private static final int REQUEST_AUTOTEST = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        regbutton1 = (Button) findViewById(R.id.regbutton);
+
+        Button regbutton;
+        regbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, code_detect.class));
+            }
+        });
     }
 }
